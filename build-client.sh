a1156f74c410eecb21a2eda95da9e2536e14a403
Zhiqiang He <rhiswell@gmail.com>
2018-12-12 13:57:18 +0800

    Fix build-client.sh

--- build-client.sh
+++ build-client.sh
@@ -1,12 +1,22 @@
-cd libraries/libmlx4-1.2.1mlnx1/
+cd libmempool/
 make
 make install
 
+cd ../libraries/libmlx4-1.2.1mlnx1/
+./autogen.sh
+CFLAGS="-Wno-error" ./configure --prefix=/usr/
+make
+make install
+
+apt -y install libnl-dev
 cd ../libibverbs-1.2.1mlnx1/
+./autogen.sh
+CFLAGS="-Wno-error" LIBS="-lmempool -lrt" ./configure --prefix=/usr/
 make
 make install
 
 cd ../librdmacm-1.1.0mlnx/
+./autogen.sh
+./configure --prefix=/usr/
 make
 make install
-
